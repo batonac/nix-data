0a0e4ec0c61bc9facceaa8bf64fb8102e60a2241
Kevin Shenk <kevin@avu.nu>
2025-12-25 03:41:11 +0000

    chore: explicit upstream flakes

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils-plus = {
+      url = "github:snowfallorg/flake-utils-plus";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils-plus.follows = "flake-utils-plus";
     };
   };
 
